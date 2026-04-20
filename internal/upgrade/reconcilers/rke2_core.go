09688c907c8f77dba39e31d785a4000706215fd4
Ivo Petrov <ivo.petrov@suse.com>
2026-04-20 09:39:45 -0400

    Better logging for RKE2 packaged components

--- internal/upgrade/reconcilers/rke2_core.go
+++ internal/upgrade/reconcilers/rke2_core.go
@@ -237,7 +237,7 @@ func (h *RKE2PackagedComponentsHandler) ReconcileAvailability(ctx context.Contex
 		if !jobComplete {
 			return &upgrade.PhaseStatus{
 				State:   lifecyclev1alpha1.UpgradeInProgress,
-				Message: fmt.Sprintf("%s RKE2 HelmChart Job execution is still in progress", pair.Chart.Name),
+				Message: fmt.Sprintf("RKE2 packaged component %s execution is still in progress", pair.Chart.Name),
 			}, nil
 		}
 	}
